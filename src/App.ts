fa3e8d807722bbf3043f20b2ab149dedf4da2acd
Valerie Fuchs <valerie.fuchs@wetransfer.com>
2020-08-24 16:51:36 +0200

    Setting up prettier for TypeScript

--- src/App.ts
+++ src/App.ts
@@ -2,13 +2,13 @@ import express = require("express");
 import routes from "./routes/";
 
 class App {
-	public app: express.Application;
-	constructor() {
-		this.app = express();
-		this.config();
-	}
-	private config(): void {
-		this.app.use("/", routes);
-	}
+  public app: express.Application;
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+  private config(): void {
+    this.app.use("/", routes);
+  }
 }
 export default new App().app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,8 @@ import { Request, Response } from "express";
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response ) => {
-    return res.send('Hallo');
-})
+router.get("/", (req: Request, res: Response) => {
+  return res.send("Hallo");
+});
 
 export default router;
